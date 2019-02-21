1853bf06bc2daa0ca7a3f30b8d6a5501fab51848
Pavlo Tokariev <origbluebullet@gmail.com>
2019-02-21 20:28:01 +0200

    Improve readability.

--- network-1.py
+++ network-1.py
@@ -7,9 +7,9 @@ import networkx as nx
 
 parser = argparse.ArgumentParser(description='Simulates "broadcast storm" and shows growth plot of messages in it.\n'
                                              'Be careful with arguments, messages grow really fast.')
-parser.add_argument('-n', '--vertices', type=int, nargs='?', default=3,
+parser.add_argument('-n', '--vertices', type=int, nargs='?', default=5,
                     help='amount of vertices in network of complete graph')
-parser.add_argument('-s', '--steps', type=int, nargs='?', default=6,
+parser.add_argument('-s', '--steps', type=int, nargs='?', default=10,
                     help='amount of steps in a simulation')
 
 args = parser.parse_args()
@@ -25,14 +25,16 @@ for node, attributes in Net.nodes.items():
 message_count = 1
 results: List[int] = [1]
 for i in range(1, steps):
-    for node, p_attrs in Net.nodes(data=True):
+    for node, parent_attrs in Net.nodes(data=True):
+        parent_messages = parent_attrs['messages']
+        parent_attrs['messages'] = 0
+        message_count -= parent_messages
         for neighbour in Net[node]:
             c_attrs = Net.node[neighbour]
-            c_attrs['messages'] += p_attrs['messages']
-            message_count += p_attrs['messages']
-        p_attrs['messages'] = 0
+            c_attrs['messages'] += parent_messages
+            message_count += parent_messages
     results.append(message_count)
 
-print(results)
+print('Step message counts: ', ', '.join(map(str, results)))
 plt.plot(list(range(steps)), results)
 plt.show()
