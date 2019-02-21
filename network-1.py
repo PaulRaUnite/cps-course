5edf6bed65aa00810a86291dc699e3934460694e
Pavlo Tokariev <origbluebullet@gmail.com>
2019-02-21 20:23:06 +0200

    Model optimization.

--- network-1.py
+++ network-1.py
@@ -1,13 +1,9 @@
 import argparse
 import random
-from collections import namedtuple
 from typing import List
 
-import networkx as nx
 import matplotlib.pyplot as plt
-
-Message = namedtuple('Message', ())
-Measure = namedtuple('Measure', ('step', 'count'))
+import networkx as nx
 
 parser = argparse.ArgumentParser(description='Simulates "broadcast storm" and shows growth plot of messages in it.\n'
                                              'Be careful with arguments, messages grow really fast.')
@@ -24,18 +20,19 @@ initiator = random.randrange(n)
 
 Net = nx.complete_graph(n)
 for node, attributes in Net.nodes.items():
-    attributes['messages'] = [] if node != initiator else [Message()]
+    attributes['messages'] = int(node == initiator)
 
 message_count = 1
-results: List[Measure] = [Measure(step=0, count=1)]
+results: List[int] = [1]
 for i in range(1, steps):
     for node, p_attrs in Net.nodes(data=True):
         for neighbour in Net[node]:
             c_attrs = Net.node[neighbour]
-            c_attrs['messages'].extend(p_attrs['messages'])
-            message_count += len(p_attrs['messages'])
-        p_attrs['messages'] = []
-    results.append(Measure(step=i, count=message_count))
+            c_attrs['messages'] += p_attrs['messages']
+            message_count += p_attrs['messages']
+        p_attrs['messages'] = 0
+    results.append(message_count)
 
-plt.plot(*zip(*results))
+print(results)
+plt.plot(list(range(steps)), results)
 plt.show()
