a304608f7c39487c44a0d235f9ac4776f6585d96
Pavlo Tokariev <origbluebullet@gmail.com>
2019-03-03 15:20:25 +0200

    Model enhancements and fixes.

--- network-4.py
+++ network-4.py
@@ -14,12 +14,15 @@ parser.add_argument('-v', '--vertices', type=int, nargs='?', default=5,
                     help='amount of vertices in network of complete graph, default is 5')
 parser.add_argument('-p', '--probability', type=float, nargs='?', default=0.4,
                     help='probability of edge generation, default is 0.4')
-parser.add_argument('-n', '--steps', type=int, nargs='?', default=1000,
-                    help='number of steps in simulation')
-parser.add_argument('-b', '--broadcast', type=float, nargs='?', default=0.1,
-                    help='broadcast probability')
+parser.add_argument('-n', '--steps', type=int, nargs='?', default=100,
+                    help='number of steps in simulation, default is 100')
+parser.add_argument('-b', '--broadcast', type=float, nargs='?', default=0.2,
+                    help='broadcast spawn probability, default is 0.2')
 parser.add_argument('-s', '--seed', type=int, nargs='?', default=None,
-                    help='seed of generation, use it if you want to reproduce some outcome')
+                    help='seed of generation, use it if you want to reproduce some outcome, '
+                         'other characteristics should be the same too')
+parser.add_argument('-d', '--debug', type=bool, nargs='?', default=False,
+                    help='print statistics')
 
 message_type = count()
 
@@ -30,6 +33,7 @@ v = args.vertices
 seed = args.seed
 steps = args.steps
 broadcast_probability = args.broadcast
+debug = args.debug
 
 if not seed:
     seed = random.randrange(9999)
@@ -43,14 +47,17 @@ for node, attrs in Net.nodes.items():
     attrs['broadcast_table']: Dict[int, int] = {}
 
 for _, channel in Net.edges.items():
-    channel['messages'] = []
+    channel['sent'] = []
+    channel['planned'] = []
 
 
 def launch_broadcast_from(node):
     message = next(message_type)
-    print("Initiator: ", node, ", message: ", message)
+    if debug:
+        print("Initiator: ", node, ", message: ", message)
     for _, channel in Net[node].items():
-        channel['messages'].append(message)
+        channel['sent'].append(message)
+    Net.nodes[node]['broadcast_table'][message] = 2
 
 
 def try_to_launch_new_broadcast():
@@ -61,23 +68,25 @@ def try_to_launch_new_broadcast():
 
 def process_messages():
     for (source, repeater), downstream in Net.edges.items():
-        for message in downstream['messages']:
+        for message in downstream['sent']:
             if message in Net.nodes[repeater]['broadcast_table']:
                 continue
             for target, upstream in Net[repeater].items():
                 if target == source:
                     continue
-                upstream['messages'].append(message)
+                upstream['planned'].append(message)
             Net.nodes[repeater]['broadcast_table'][message] = 2
-        downstream['messages'] = []
     for node, attrs in Net.nodes.items():
         attrs['broadcast_table'] = {
             message: live_time - 1 for message, live_time in attrs['broadcast_table'].items() if live_time != 0
         }
+    for _, channel in Net.edges.items():
+        channel['sent'] = channel['planned']
+        channel['planned'] = []
 
 
 def collect_results() -> (Mapping[int, int], int):
-    return Counter(chain.from_iterable(ch['messages'] for ch in Net.edges.values())), \
+    return Counter(chain.from_iterable(ch['sent'] for ch in Net.edges.values())), \
            sum(len(node_data['broadcast_table']) for node_data in Net.nodes.values())
 
 
@@ -87,7 +96,8 @@ for _ in range(steps):
     try_to_launch_new_broadcast()
     process_messages()
     r = collect_results()
-    print(r)
+    if debug:
+        print(r)
     statistics.append(r)
 
 plt.subplot(1, 2, 1)
@@ -105,7 +115,7 @@ plt.subplot(1, 2, 2)
 for message, count in lines_data.items():
     plt.plot(x, count, label="message {}".format(message))
 
-plt.plot(x, [table_length for _, table_length in statistics], label="Length of tables")
+plt.plot(x, [table_length for _, table_length in statistics], linestyle='-.', label="General length of message tables.")
 plt.legend()
 plt.xlabel('Steps')
 plt.ylabel('Messages')
